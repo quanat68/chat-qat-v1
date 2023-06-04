7787c3bf5455965c714b67f54e10eaedb74804dc
quanat68 <trinhanhquan6801@gmail.com>
2023-06-05 05:00:42 +0700

    Updates

---
+++ precache-manifest.1cf67ee9d96f3313268f1f4aa7e4b3ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d83359227f389df205a31fd7f2dbabc8",
+    "url": "/chat-qat-v1/index.html"
+  },
+  {
+    "revision": "3d3fa9faaff049504ea9",
+    "url": "/chat-qat-v1/static/css/main.b572f603.chunk.css"
+  },
+  {
+    "revision": "f4f275dce4418bb86369",
+    "url": "/chat-qat-v1/static/js/2.7dff0eff.chunk.js"
+  },
+  {
+    "revision": "563cec0a8f134cb3c9b5211a1992f1ed",
+    "url": "/chat-qat-v1/static/js/2.7dff0eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d3fa9faaff049504ea9",
+    "url": "/chat-qat-v1/static/js/main.1b5074f1.chunk.js"
+  },
+  {
+    "revision": "f7e43ce04c1757cac3f7",
+    "url": "/chat-qat-v1/static/js/runtime-main.17347c77.js"
+  }
+]);
